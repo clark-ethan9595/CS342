5afb84f0142cf5e4039b551184b71f34a220705d
Ethan Clark <elc3@students.calvin.edu>
2017-02-12 23:32:02 -0500

    Updated Guide3 and Guide3 diagrams

---
+++ 02modeling/homework02.sql
@@ -0,0 +1,23 @@
+-- Homework 2 - CS342
+-- Ethan Clark
+
+-- 1. Exercise 5.14
+	-- Implement this schema using the Oracle SQL DDL. Please add an item name in the Item table.
+	-- Include appropriate constraints and, in particular, specifiy what should be done on deletes (e.g. cascade...).
+	--		Justify your implementation in the code comments.
+	-- Include 3-5 sample records in each table. You'll need to have sufficient records to drive the queries below,
+	--		so it's probably wise to look through the queries so that you will have non-empty and non-NULL results for each of them.
+
+-- 2. Exercise 5.20 a. and c.
+	-- Consider only this modified version of the text exercise - What recommendations would you have for CIT if they were considering replacing surrogate student ID numbers with a more natural key?
+	--		Either suggest a new form of key or try to convince them that surrogate keys are acceptable.
+
+-- 3. Write the SQL commands to retrieve the following from the customer-order database you built above.
+	-- a. All the order dates and amounts for orders made by a customer with a particular name (one that exists in your database),
+	--		ordered chronologically by date
+	
+	-- b. All the customer ID numbers for customers who have made at least one order in the database
+	
+	-- c. The customer IDs and names of the people who have ordered an item with a particular name (one that exists in your database)
+
+-- 4. Complete lab exercises 2.2 and 2.3
