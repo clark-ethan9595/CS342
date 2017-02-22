ff6cd81569d2bed2deaa6c23dae0b4e26b734f64
Ethan Clark <elc3@students.calvin.edu>
2017-02-21 23:14:53 -0500

    Updated Homework3

--- 03modeling/homework03_3.sql
+++ 03modeling/homework03_3.sql
@@ -3,3 +3,59 @@
 
 -- Ethan Clark - CS 342
 -- February 22, 2017
+
+-- drop current database tables
+DROP TABLE Part_Orders;
+DROP TABLE Handles;
+DROP TABLE Orders;
+DROP TABLE Part;
+DROP TABLE Customer;
+DROP TABLE Employee;
+
+-- Create database tables
+CREATE TABLE Employee (
+	empNumber integer PRIMARY KEY,
+	zipCode integer,
+	firstName varchar(20),
+	lastName varchar(20)
+);
+
+CREATE TABLE Customer (
+	custNumber integer PRIMARY KEY,
+	firstName varchar(20),
+	lastName varchar(20),
+	zipCode integer
+);
+
+CREATE TABLE Part (
+	partNumber integer PRIMARY KEY,
+	name varchar(30),
+	price float,
+	stockQty integer
+);
+
+CREATE TABLE Orders (
+	orderNumber integer PRIMARY KEY,
+	datePlaced date,
+	expectedShipDate date,
+	actualShipDate date
+);
+
+CREATE TABLE Handles (
+	empNumber integer,
+	custNumber integer,
+	orderNumber integer,
+	FOREIGN KEY (empNumber) REFERENCES Employee(empNumber),
+	FOREIGN KEY (custNumber) REFERENCES Customer(custNumber),
+	FOREIGN KEY (orderNumber) REFERENCES Orders(orderNumber),
+	PRIMARY KEY (empNumber, custNumber, orderNumber)
+);
+
+CREATE TABLE Part_Orders (
+	partNumber integer,
+	orderNumber integer,
+	quantity integer,
+	FOREIGN KEY (partNumber) REFERENCES Part(partNumber),
+	FOREIGN KEY (orderNumber) REFERENCES Order(orderNumber),
+	PRIMARY KEY (partNumber, orderNumber)
+);
--- 03modeling/lab03.sql
+++ 03modeling/lab03.sql
@@ -8,7 +8,7 @@
 -- Homework3 - Lab Exercise 3.3
 
 -- drop current database
-DROP TABLE Mentorship;
+DROP TABLE Request;
 DROP TABLE Person_Team;
 DROP TABLE Team;
 DROP TABLE Person;
@@ -43,20 +43,10 @@ CREATE TABLE Person (
 	role varchar(20),
 	membershipStatus char(1) CHECK (membershipStatus IN ('m', 'a', 'c')),
 	homegroupID integer,
-	FOREIGN KEY (householdID) REFERENCES HouseHold(ID) ON DELETE SET NULL,
-	FOREIGN KEY (homegroupID) REFERENCES Homegroup(ID) ON DELETE SET NULL
-	);
-	
--- I think that setting the menteeID to be the primary key would not allow a person to
---		have more than one mentor. This schema would allow for a person to be a mentor
---		to more than one person.
--- If a person is removed from the database, we would want to remove the records of them
---		in this table as well.
-CREATE TABLE Mentorship (
 	mentorID integer,
-	menteeID integer PRIMARY KEY,
-	FOREIGN KEY (mentorID) REFERENCES Person(ID) ON DELETE CASCADE,
-	FOREIGN KEY (menteeID) REFERENCES Person(ID) ON DELETE CASCADE
+	FOREIGN KEY (householdID) REFERENCES HouseHold(ID) ON DELETE SET NULL,
+	FOREIGN KEY (homegroupID) REFERENCES Homegroup(ID) ON DELETE SET NULL,
+	FOREIGN KEY (mentorID) REFERENCES Person(ID) ON DELETE SET NULL
 	);
 	
 CREATE TABLE Team (
@@ -79,24 +69,42 @@ CREATE TABLE Person_Team (
 	CONSTRAINT PK_D PRIMARY KEY (personID, teamID)
 	);
 
+-- If a particular household gets deleted from the database, we would want to remove their
+--		request that they submitted because it would no longer have a place it came from.
+-- If a person is deleted from the database and is linked as a responder, I feel like the
+--		request itself should not be deleted so the responderID should be set to NULL so that
+--		the church could figure out what to do with it.
+CREATE TABLE Request (
+	submitDate date,
+	text_char varchar(50),
+	access varchar(50),
+	response varchar(50),
+	requestorID integer,
+	responderID integer,
+	FOREIGN KEY (requestorID) REFERENCES Household(ID) ON DELETE CASCADE,
+	FOREIGN KEY (responderID) REFERENCES Person(ID) ON DELETE SET NULL,
+	PRIMARY KEY (submitDate)
+	);
+
 -- Insert sample data
 INSERT INTO Household VALUES (0,'2347 Oxford Dr. SE','Grand Rapids','MI','49506','616-243-5680');
 INSERT INTO Household VALUES (1,'2632 Colton Dr. SE','Grand Rapids','MI','49506','616-888-8888');
 
 INSERT INTO Homegroup VALUES (0, 'coffee shop');
 
-INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden', 0, 'husband', 'm', 0);
-INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden', 0, 'wife', 'm', 0);
-INSERT INTO Person VALUES (2,'mr.','Ethan','Clark', 1, 'son', 'm', NULL);
-
-INSERT INTO Mentorship VALUES (0, 1);
-INSERT INTO Mentorship VALUES (0, 2);
+INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden', 0, 'husband', 'm', 0, NULL;
+INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden', 0, 'wife', 'm', 0, 0);
+INSERT INTO Person VALUES (2,'mr.','Ethan','Clark', 1, 'son', 'm', NULL, 0);
 
 INSERT INTO Team VALUES (0, 'Elders');
 INSERT INTO Team VALUES (1, 'Music');
 
 INSERT INTO Person_Team VALUES (2, 0, 'Outreach', '01-Feb-2017', '01-Oct-2017');
 INSERT INTO Person_Team VALUES (0, 1, 'Leader', '19-Jan-2017', '19-Jul-2017');
+
+INSERT INTO Request VALUES ('05-Sept-2016', 'Please pray for my family', 'placeholder', 'Know we are praying', 0, 0);
+INSERT INTO Request VALUES ('12-Nov-2016', 'Please pray for my sister', 'placeholder text', 'Know we are praying for you', 0, 1);
+INSERT INTO Request VALUES ('30-Jan-2016', 'My grandma passed away', 'text placeholder', 'We are sorry for your loss', 1, 2);
 
 -- Sample queries
 SELECT * FROM Homegroup;
@@ -108,3 +116,9 @@ WHERE Person.ID = Person_Team.personID AND Team.ID = Person_Team.teamID;
 
 SELECT Person.firstName, Person_Team.endDate FROM Person, Person_Team
 WHERE Person.ID = Person_Team.personID ORDER BY Person_Team.endDate ASC;
+
+SELECT * FROM Request;
+
+SELECT submitDate, text_char, ID FROM Household, Request
+WHERE Household.ID = Request.requestorID AND Household.ID = 0
+ORDER BY submitDate DESC;
