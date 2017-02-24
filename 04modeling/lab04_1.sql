457cc588731f038da313550e58a2fa31fb523ab8
Ethan Clark <elc3@calvin.edu>
2017-02-24 12:09:05 -0500

    Begun Lab4

---
+++ 04modeling/lab04_1.sql
@@ -0,0 +1,68 @@
+-- This command file loads an experimental person relation.
+-- The data conforms to the following assumptions:
+--     * Person IDs and team names are unique for people and teams respectively.
+--     * People can have at most one mentor.
+--     * People can be on many teams, but only have one role per team.
+--     * Teams meet at only one time.
+--
+-- CS 342
+-- Spring, 2017
+-- kvlinden
+
+drop table AltPerson;
+
+CREATE TABLE AltPerson (
+	personId integer,
+	name varchar(10),
+	status char(1),
+	mentorId integer,
+	mentorName varchar(10),
+	mentorStatus char(1),
+    teamName varchar(10),
+    teamRole varchar(10),
+    teamTime varchar(10)
+	);
+
+INSERT INTO AltPerson VALUES (0, 'Ramez', 'v', 1, 'Shamkant', 'm', 'elders', 'trainee', 'Monday');
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'elders', 'chair', 'Monday');
+INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'executive', 'protem', 'Wednesday');
+INSERT INTO AltPerson VALUES (2, 'Jennifer', 'v', 3, 'Jeff', 'm', 'deacons', 'treasurer', 'Tuesday');
+INSERT INTO AltPerson VALUES (3, 'Jeff', 'm', NULL, NULL, NULL, 'deacons', 'chair', 'Tuesday');
+
+
+-- Exercise 4.1
+-- a.
+-- Informally, this relation is not well-designed because there are numerous NULL values
+--		that are found in the data. This wastes space at the storage level and may also lead
+--		to problems with understanding the meaning of those attributes. Also, there is a large
+--		amount of redundant information. With team name, every time someone new gets added
+--		to that team, the team name will need to be repeated, wasting space. We also have to
+--		repeat the personId, name, and status for every time someone joins a new team.
+-- To prove this formally, let us take a look at whether this relation is in BCNF. For BCNF
+--		all left hand sides of functional dependencies must be a superkey of the relation. There is
+--		a functional dependency teamName -> teamRole. There is another functional dependency personId
+--		-> name. However, the Left Hand Side of these functional dependencies are NOT superkeys. The 
+-- 		superkey for this relation is personId, teamName together. Just personId alone is not a superkey
+--		and just teamName alone is not a superkey either.
+
+-- b.
+-- A more properly normalized schema for this database would be:
+--		Person(personId, name, status, mentorId)
+--		Team(teamId, teamName)
+--		Person_Team(personId, teamId, role, time)
+--
+-- In this schema, we reduce the number of NULL values by having the mentorId as a foreign key
+--		to Person(personId), so there is no need to store mentorName and mentorStatus along
+--		with the mentorship information. We also reduce redundant values by creating a Team
+--		relation that stores information for each team only once, not many times for each Person
+--		on that team. We only enter in information when someone actually belongs to a team in the
+--		Person_Team relation.
+--
+-- This schema is also in BCNF because, for each functional dependency in each table, the left hand side
+--		of that functional dependency is a superkey for  that relation.
+--		Examples:
+--			personId -> name, status, mentorId
+--			teamId -> teamName
+--			personId, teamId -> role, time
+--		personId is a superkey for the Person relation, teamId is a superkey for the Team relation,
+--		and personId, teamId is a superkey for the Person_Team relation.
---
+++ 04modeling/lab04_2.sql
@@ -0,0 +1,62 @@
+-- This command file loads an experimental person database.
+-- The data conforms to the following assumptions:
+--     * People can have 0 or many team assignments.
+--     * People can have 0 or many visit dates.
+--     * Teams and visits don't affect one another.
+--
+-- CS 342, Spring, 2017
+-- kvlinden
+
+DROP TABLE PersonTeam;
+DROP TABLE PersonVisit;
+
+CREATE TABLE PersonTeam (
+	personName varchar(10),
+    teamName varchar(10)
+	);
+
+CREATE TABLE PersonVisit (
+	personName varchar(10),
+    personVisit date
+	);
+
+-- Load records for two team memberships and two visits for Shamkant.
+INSERT INTO PersonTeam VALUES ('Shamkant', 'elders');
+INSERT INTO PersonTeam VALUES ('Shamkant', 'executive');
+INSERT INTO PersonVisit VALUES ('Shamkant', '22-FEB-2015');
+INSERT INTO PersonVisit VALUES ('Shamkant', '1-MAR-2015');
+
+-- Query a combined "view" of the data of the form View(name, team, visit).
+SELECT pt.personName, pt.teamName, pv.personVisit
+FROM PersonTeam pt, PersonVisit pv
+WHERE pt.personName = pv.personName;
+
+-- Exercise 4.2
+-- a.
+-- There are no functional dependencies in either the PersonTeam or the PersonVisit tables.
+--		A given person can be on multiple teams and a team can have multiple people
+--		so neither personName functionally depends teamName nor teamName functionally depends
+--		personName. Also, a given person can visit a church multiple times so neither personName
+--		functionally depends personVisit nor personVisit functionally depends personName. This
+--		means that both relations are in BCNF form because there are no functional dependencies whose
+--		left hand side is not a superkey.
+-- There are also no multivalued dependencies in either the PersonTeam or the PersonVisit tables.
+--		There are no situations where one attribute depends on another with respect to a third attribute
+--		because there are only two attributes in each table. So there are no multivalued dependencies
+--		where the left hand side is not a superkey, meaning both tables are in 4NF.
+
+-- b.
+-- The output of the combined "view" query, when taken as a single relation, is in BCNF. Again, for
+--		BCNF, all functional dependencies must have the left hand side be a superkey. There are actually
+--		no functional dependencies in this single relation because no attribute functionally depends
+--		on another attribute, e.g. personName does not determine teamName or personVisit.
+-- However, the output of this single relation is NOT in 4NF. To be in 4NF, every multivalued dependency
+--		left hand side must be a superkey of the relation. There is a multivalued dependency of personName ->>
+--		teamName | personVisit. However, personName alone is not a superkey. The superkey for this relation is
+--		personName, teamName, personVisit all together. Because that one multivalued dependency violates the left
+--		hand side condition, this combined "view" single relation is not in 4NF.
+
+-- c.
+-- Just because the original schema and the derived "view" have the same number of records does not
+--		mean they are equally appropriate. The single "view" relation needs to have every combination of records
+--		to make up all the data/information for that relation.
