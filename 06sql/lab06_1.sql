55a6ea354710e207bd3dbb4dccfa912ef766b2cd
Ethan Clark <Ethan Clark>
2017-03-09 22:56:02 -0500

    started lab06

---
+++ 06sql/lab06_1.sql
@@ -0,0 +1,9 @@
+-- Lab06_1.sql
+-- Ethan Clark
+-- CS 342 - Lab06
+-- March 10, 2017
+
+-- Exercise 6.1
+-- a.
+
+-- b.
---
+++ 06sql/lab06_2.sql
@@ -0,0 +1,11 @@
+-- Lab06_2.sql
+-- Ethan Clark
+-- CS 342 - Lab06
+-- March 10, 2017
+
+-- Exercise 6.2
+-- a.
+
+-- b.
+
+-- c.
