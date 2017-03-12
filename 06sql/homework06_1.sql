f0bcafc36cfbf51822f6da6432e32cd7cc4906d2
Ethan Clark <elc3@students.calvin.edu>
2017-03-12 16:21:20 -0400

    Guide07 complete

---
+++ 06sql/homework06_1.sql
@@ -0,0 +1,20 @@
+-- Homework06_1.sql
+-- Ethan Clark
+-- CS 342
+-- March 11, 2017
+
+-- Exercise 1
+-- Please write SQL queries that retrieve the following information from the HR database.
+
+-- a. The IDs and full names of managers and the number of employees each of them manages. Order the results by decreasing number
+--		of employees and return only the top ten results.
+
+-- b. THe name, number of employees and total salary for each department outside of the US with less than 100 employees. The total
+--		salary is the sum of salaries of each of the department's employees.
+
+-- c. The department name, department manager name and manager job title for all departments. If the department has no manager,
+--		include it in the output with NULL values for the manager and title.
+
+-- d. The name of each department along with the average salary of the employees of that department. If a department has no employees,
+--		include in the list with no salary value. Order your results by decreasing salary. You may order the NULL valued salaries
+--		however you'd like.
