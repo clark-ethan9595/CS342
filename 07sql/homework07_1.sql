48e8040ba3d327c970f47b3271173701243ba039
Ethan Clark <elc3@students.calvin.edu>
2017-03-26 21:52:50 -0400

    Started Guide08

---
+++ 07sql/homework07_1.sql
@@ -0,0 +1,14 @@
+-- Homeowork07_1.sql
+-- Ethan Clark
+-- CS 342 - Lab07
+-- March 27, 2017
+
+-- Exercise 1
+
+-- a.
+
+-- b.
+
+-- c.
+
+-- d.
---
+++ 07sql/homework07_2.sql
@@ -0,0 +1,14 @@
+-- Homework07_2.sql
+-- Ethan Clark
+-- CS 342 - Lab07
+-- March 27, 2017
+
+-- Exercise 2
+
+-- a.
+
+-- b.
+
+-- c.
+
+-- d.
