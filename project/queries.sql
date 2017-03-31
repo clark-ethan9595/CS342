ca70812805a127ee5a22f384cd9366d7d63ca0c5
Ethan Clark <elc3@calvin.edu>
2017-03-31 16:25:11 -0400

    Project 3 Finished

--- project/queries.sql
+++ project/queries.sql
@@ -1,37 +1,49 @@
 -- queries.sql contains a set of queries that will be useful for my system
 -- CS 342 - Final Project
 -- Ethan Clark (elc3)
--- March 27, 2017
+-- March 31, 2017
 
 
--- This query uses two aggregate functions (TRUNC and AVG) to calculate the average statistics for each player.
--- 		It (inner) joins the athlete table with the athlete_game table.
--- 		We need to group by athleteId and lastName because those are both not aggregate functions in the select clause.
--- 		We order by athleteId (default ascending) which will group the players by school.
-SELECT A.athleteId, A.lastName, TRUNC(AVG(AG.points), 1) AS PPG, TRUNC(AVG(AG.rebounds), 1) AS RPG, TRUNC(AVG(AG.assists), 1) AS APG,
-		TRUNC(AVG(AG.steals), 1) AS SPG, TRUNC(AVG(AG.blocks), 1) AS BPG, TRUNC(AVG(AG.turnovers), 1) AS TPG
-FROM Athlete_Game AG, Athlete A
-WHERE AG.athleteId = A.athleteId
-GROUP BY A.athleteId, A.lastName
+-- QUERY 1
+-- This query returns the average statistics for all the athletes in the database, ordered by athleteId number and grouped by school.
+--		The users of the fantasy league would care about these results when they are drafting players to be on their fantasy team.
+--
+-- This query makes use of aggregate functions and inner joins.
+SELECT A.athleteId, S.name, A.lastName, COUNT(AG.points) AS Games_Played, TRUNC(AVG(AG.points), 1) AS PPG, TRUNC(AVG(AG.rebounds), 1) AS RPG,
+		TRUNC(AVG(AG.assists), 1) AS APG, TRUNC(AVG(AG.steals), 1) AS SPG, TRUNC(AVG(AG.blocks), 1) AS BPG, TRUNC(AVG(AG.turnovers), 1) AS TPG
+FROM Athlete_Game AG, Athlete A, School S
+WHERE AG.athleteId = A.athleteId AND A.schoolId = S.schoolId
+GROUP BY A.athleteId, A.lastName, S.name
 ORDER BY A.athleteId;
+-- I do not think there is another way that this query can be written.
 
 
--- This query shows all the athletes that belong to a fantasy team along with:
--- 		the User's username, the User's team name, the Team's league name, the
--- 		athlete's first and last name, and the school the athlete goes to.
--- 		This query needed to join six different tables to get the necessary information.
+-- QUERY 2
+-- This query shows all the athletes that belong to a fantasy team along with the user's username, the user's team name, 
+--		the league name for that team, the athlete's first and last name, and the school the athlete goes to.
+--
+-- Fantasy league users (and maybe the league administrator) would care to use this query to view all the athletes that are
+--		on a fantasy team within a given league.
+--
+-- This query joins six tables together to get the desired results.
 SELECT U.userName, T.teamName, L.leagueName, A.firstName, A.lastName, S.name
-FROM Users U, Team T, Athlete_Team AT, League L, Athlete A, School S
+FROM Users U, Team T, Athlete_Team AT1, League L, Athlete A, School S
 WHERE U.userId = T.userId AND T.leagueId = L.leagueId
-AND AT.teamId = T.teamId AND AT.athleteId = A.athleteId
+AND AT1.teamId = T.teamId AND AT1.athleteId = A.athleteId
 AND A.schoolId = S.schoolId;
+-- This query could probably be written in different ways using different kinds of joins. I choose to write it this way
+--		for simplicity sake.
 
 
--- This query returns all the athletes who are not part of a fantasy team.
--- 		This query needs a nested select statement and it is not correlated, meaning
--- 		I could run the inner query first and then run the outer query on the results of the
--- 		inner query.
-SELECT athleteId
+-- QUERY 3
+-- This query returns all the athletes who are not part of any fantasy team in any league.
+--
+-- League administrators and users would like to use this query to give out statistics about
+--		which players are not part of a fantasy team in any league.
+--
+-- This query needs a nested select statement to find the players not in the result set of all
+--		players that belong to a fantasy team.
+SELECT athleteId, firstName, lastName
 FROM Athlete
 WHERE athleteId NOT IN (
 	SELECT A.athleteId
@@ -39,18 +51,69 @@ WHERE athleteId NOT IN (
 	WHERE AT.athleteId = A.athleteId
 )
 ORDER BY athleteId;
+-- I think that using a sub select to get all of the athletes on a fantasy team and then finding the athletes NOT IN
+--		that result is the only way to do this query.
 
 
--- This query returns the athletes who have not played at least one game
--- 		(i.e. they have NULL values in their game data). This query needs
--- 		the use of IS NULL to compare to a NULL value.
-SELECT DISTINCT A.athleteId, A.firstName, A.lastName, AG.gameId
-FROM Athlete A, Athlete_Game AG
-WHERE A.athleteId = AG.athleteId AND AG.points IS NULL;
-
-
-SELECT A.athleteId, A.lastName, COUNT(*)
+-- QUERY 4
+-- This query simply returns the number of games each athlete has played.
+--
+-- This query would be important to both fantasy league users and potentially for
+--		the basketball teams in the MIAA conference.
+--
+-- This query uses proper comparison of NULL values (IS NOT NULL) and uses inner joins.
+SELECT A.athleteId, A.lastName, COUNT(*) AS Games_Played
 FROM Athlete A, Athlete_Game AG
 WHERE A.athleteId = AG.athleteId AND AG.points IS NOT NULL
 GROUP BY A.athleteId, A.lastName
 ORDER BY COUNT(*) DESC;
+-- This query could be written using any comparison of AG.points, AG.rebounds, AG.steals, etc. because
+--		they are all NULL values when an athlete does not play in a game. So it really does not matter
+--		which of those attributes I choose to compare with NULL.
+
+
+-- QUERY 5
+-- This query returns all the fantasy team information for each league, even if the team is not in a
+--		league yet, in order of team's record (going by wins first, then ties).
+--
+-- This query is important for users to see which fantasy team is winning its league, as well as seeing which teams need
+--		to join a league.
+--
+-- This query utilizes a LEFT OUTER JOIN so that even though a team may not be a part of a league yet, it will still show
+-- 		up in this query result set (i.e. team with ID 21 has no league, but still shows up in the results).
+SELECT L.leagueName, T.teamName, T.wins, T.losses, T.ties
+FROM Team T LEFT OUTER JOIN League L ON T.leagueId = L.leagueId
+ORDER BY L.leagueId, T.wins DESC, T.ties DESC;
+-- I think this is the only way to do this query because a LEFT OUTER JOIN is necessary to show the teams
+--		that are not in a league yet.
+
+
+-- QUERY 6
+-- This query returns a pair of athletes on the same team that both average at least 30 points per game.
+--
+-- This query would be important to users who want to know if any school has a pair of good athletes on their team.
+--
+-- This query uses a lot of different things. It uses TRUNC and AVG aggregate functions, a self-join of the athlete table, inner joins,
+--		and two HAVING clauses to work with the aggregate functions.
+SELECT A1.firstName, A1.lastName, TRUNC(AVG(AG1.points), 1) AS PPG, A2.firstName, A2.lastName, TRUNC(AVG(AG2.points), 1) AS PPG, S.name
+FROM Athlete A1, Athlete A2, School S, Athlete_Game AG1, Athlete_Game AG2
+WHERE A1.schoolId = A2.schoolId AND A1.schoolId = S.schoolId AND A1.athleteId = AG1.athleteId
+AND A2.athleteId = AG2.athleteId AND A1.athleteId > A2.athleteId
+HAVING AVG(AG1.points) >= 30 AND AVG(AG2.points) >= 30
+GROUP BY A1.firstName, A1.lastName, A2.firstName, A2.lastName, A2.position, S.name;
+-- I think this is the only reasonable way to go about solving this type of query problem.
+
+
+-- View 1
+-- This view provides a quick view of each user and their fantasy team information, including
+--		those users that do not have a fantasy team yet.
+--
+-- The fantasy league administrators would use this view in order to see which users need to create a team and
+--		join a league to get started.
+--
+-- I created this view as a non-materialized view because updates should happen to the base tables, so then this
+--		view would simply rerun the view query to get the updated information. All of the information in this table
+--		should get updated through the base tables it comes froms, not the view itself.
+CREATE VIEW teamUser AS
+	SELECT U.userName, T.teamName, T.wins, T.losses, T.ties
+	FROM Users U LEFT OUTER JOIN Team T ON U.userId = T.userId;
--- project/schema.sql
+++ project/schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE School (
 	state_char varchar(2)
 );
 
+-- If a School gets deleted from the database, then it makes sense for the Athletes to
+--		be removed as well.
 CREATE TABLE Athlete (
 	athleteId integer PRIMARY KEY,
 	firstName varchar(15),
@@ -18,16 +20,18 @@ CREATE TABLE Athlete (
 	grade varchar(10),
 	position varchar(10),
 	schoolId integer,
-	FOREIGN KEY (schoolId) REFERENCES School(schoolId)
+	FOREIGN KEY (schoolId) REFERENCES School(schoolId) ON DELETE CASCADE
 );
 
+-- If a School gets deleted from the database, then I think a Game should be set to NULL because
+--		then the School remaining could still find another School to potentially play.
 CREATE TABLE Game (
 	gameId integer PRIMARY KEY,
 	matchDate date,
 	homeSchoolId integer,
 	awaySchoolId integer,
-	FOREIGN KEY (homeSchoolId) REFERENCES School(schoolId),
-	FOREIGN KEY (awaySchoolId) REFERENCES School(schoolId)
+	FOREIGN KEY (homeSchoolId) REFERENCES School(schoolId) ON DELETE SET NULL,
+	FOREIGN KEY (awaySchoolId) REFERENCES School(schoolId) ON DELETE SET NULL
 );
 
 CREATE TABLE League (
@@ -43,6 +47,10 @@ CREATE TABLE Users (
 	lastName varchar(20)
 );
 
+-- If a User gets deleted from the database, then the Team that belonged to them should also get deleted.
+--
+-- If a League gets deleted from the database, then the Teams in that league should NOT get deleted, but we
+--		can set their leagueId to be NULL because we can have Teams not in a League.
 CREATE TABLE Team (
 	teamId integer PRIMARY KEY,
 	teamName varchar(50),
@@ -51,18 +59,27 @@ CREATE TABLE Team (
 	ties integer,
 	userId integer,
 	leagueId integer,
-	FOREIGN KEY (userId) REFERENCES Users(userId),
-	FOREIGN KEY (leagueId) REFERENCES League(leagueId)
+	FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
+	FOREIGN KEY (leagueId) REFERENCES League(leagueId) ON DELETE SET NULL
 );
 
+-- If an Athlete is deleted from the database, then the record of the Athlete being on a given Team
+--		should also be deleted.
+-- If a Team is deleted from the database, then the record of the Athletes being on that Team should
+--		also be deleted. This does not remove the Athletes from the Athlete table, just the Athlete_Team table.
 CREATE TABLE Athlete_Team (
 	teamId integer,
 	athleteId integer,
 	PRIMARY KEY (teamId, athleteId),
-	FOREIGN KEY (athleteId) REFERENCES Athlete(athleteId),
-	FOREIGN KEY (teamId) REFERENCES Team(teamId)
-);
+	FOREIGN KEY (athleteId) REFERENCES Athlete(athleteId) ON DELETE CASCADE,
+	FOREIGN KEY (teamId) REFERENCES Team(teamId) ON DELETE CASCADE
+);-
 
+-- If an Athlete is deleted from the database, then the records of the Games that athlete played in
+--		should also be deleted as well as they are no longer part of an MIAA school. Again, this does
+--		not remove Athletes from the Athlete table, just the Athlete_Game table.
+-- If a Game is deleted from the database, then the records of that Game should also be deleted in
+--		the Athlete_Game table as well.
 CREATE TABLE Athlete_Game (
 	gameId integer,
 	athleteId integer,
@@ -73,6 +90,6 @@ CREATE TABLE Athlete_Game (
 	blocks integer,
 	turnovers integer,
 	PRIMARY KEY (gameId, athleteId),
-	FOREIGN KEY (gameId) REFERENCES Game(gameId),
-	FOREIGN KEY (athleteId) REFERENCES Athlete(athleteId)
+	FOREIGN KEY (gameId) REFERENCES Game(gameId) ON DELETE CASCADE,
+	FOREIGN KEY (athleteId) REFERENCES Athlete(athleteId) ON DELETE CASCADE
 );
