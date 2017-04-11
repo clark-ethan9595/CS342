d0992f3c33cef4e8d539906be5b61af1c4a1e369
Ethan Clark <elc3@students.calvin.edu>
2017-04-10 23:09:01 -0400

    Project4 outline

---
+++ project/procedures.sql
@@ -0,0 +1,6 @@
+-- procedures.sql
+-- Ethan Clark
+-- CS 342 - Final Project
+-- April 11, 2017
+
+-- Create an appropriate stored procedure for your database that implements a multi-step transaction.
---
+++ project/triggers.sql
@@ -0,0 +1,7 @@
+-- triggers.sql
+-- Ethan Clark
+-- CS 342 - Final Project
+-- April 11, 2017
+
+-- Create an appropriate trigger for your database that enforces an integrity constraint (or constraints) that can’t easily be
+--		implemented with table constraints and checks. 
