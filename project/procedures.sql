d64b4776b278988886dd5d89e8e5f8c19187eef2
Ethan Clark <elc3@calvin.edu>
2017-04-20 19:44:42 -0400

    Updated for Project4

--- project/procedures.sql
+++ project/procedures.sql
@@ -1,6 +1,68 @@
 -- procedures.sql
 -- Ethan Clark
 -- CS 342 - Final Project
--- April 11, 2017
+-- April 20, 2017
 
 -- Create an appropriate stored procedure for your database that implements a multi-step transaction.
+
+-- Stored Procedure tradePlayers simulates trading two Athletes between two Fantasy Teams in the same League.
+--		It takes in as parameters two Fantasy Team ids and two Athlete ids.
+--		We first check to see if all of these Fantasy Teams and Athletes are in the database.
+--		Then we check to see if AthleteOne belongs to TeamOne and AthleteTwo belongs to TeamTwo
+--		If all of these conditions pass, then we remove the two records from the Athlete_Team table, and insert two records with
+--		the Athletes swapped on the teams.
+--
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE PROCEDURE tradePlayers (teamOneId IN INTEGER, athleteOneId IN INTEGER, teamTwoId IN INTEGER, athleteTwoId IN INTEGER) AS
+	athleteOne INTEGER;
+	athleteTwo INTEGER;
+	teamOne INTEGER;
+	teamTwo INTEGER;
+	counter INTEGER;
+BEGIN
+
+	SELECT COUNT(*) INTO athleteOne FROM Athlete WHERE athleteId = athleteOneId;
+	SELECT COUNT(*) INTO athleteTwo FROM Athlete WHERE athleteId = athleteTwoId;
+	SELECT COUNT(*) INTO teamOne FROM Team WHERE teamId = teamOneId;
+	SELECT COUNT(*) INTO teamTwo FROM Team WHERE teamId = teamTwoId;
+	
+	IF athleteOne = 0 THEN
+		dbms_output.put_line('Athlete ' || athleteOneId || ' not found.');
+	
+	ELSIF athleteTwo = 0 THEN
+		dbms_output.put_line('Athlete ' || athleteTwoId || ' not found.');
+	
+	ELSIF teamOne = 0 THEN
+		dbms_output.put_line('Fantasy Team ' || teamOneId || ' not found.');
+	
+	ELSIF teamTwo = 0 THEN
+		dbms_output.put_line('Fantasy Team ' || teamTwoId || ' not found.');
+	
+	ELSE
+
+		SELECT COUNT(*) INTO counter FROM Athlete_Team WHERE teamId = teamOneId AND athleteId = athleteOneId;
+	
+		IF counter = 0 THEN	
+			RAISE_APPLICATION_ERROR(-20000, 'Athlete ' || athleteOneId || ' is not on Fantasy Team ' || teamOneId);
+		END IF;
+	
+		SELECT COUNT(*) INTO counter FROM Athlete_Team WHERE teamId = teamTwoId AND athleteId = athleteTwoId;
+	
+		IF counter = 0 THEN
+			RAISE_APPLICATION_ERROR(-20000, 'Athlete ' || athleteTwoId || ' is not on Fantasy Team ' || teamTwoId);
+		END IF;
+		
+		DELETE FROM Athlete_Team WHERE teamId = teamOneId AND athleteId = athleteOneId;
+		
+		DELETE FROM Athlete_Team WHERE teamId = teamTwoId AND athleteId = athleteTwoId;
+		
+		INSERT INTO Athlete_Team VALUES (teamOneId, athleteTwoId);
+		
+		INSERT INTO Athlete_Team VALUES (teamTwoId, athleteOneId);
+		
+	END IF;
+
+END;
+/
+SHOW ERRORS;
--- project/triggers.sql
+++ project/triggers.sql
@@ -1,7 +1,58 @@
 -- triggers.sql
 -- Ethan Clark
 -- CS 342 - Final Project
--- April 11, 2017
+-- April 20, 2017
 
 -- Create an appropriate trigger for your database that enforces an integrity constraint (or constraints) that can’t easily be
---		implemented with table constraints and checks. 
+--		implemented with table constraints and checks.
+
+-- numPlayersTeam trigger checks to make sure that a fantasy team does not get too many players
+--		Each fantasy team can only have a maximum of 8 players
+--		If the INSERT command is going to make the team have more than 8 players, an application error is raised
+--		notifying the user of the error.
+CREATE OR REPLACE TRIGGER numPlayersTeam BEFORE INSERT ON Athlete_Team FOR EACH ROW
+DECLARE
+	 counter INTEGER;
+	 tooBigTeam EXCEPTION;	 
+BEGIN
+
+	SELECT COUNT(*) INTO counter FROM Athlete_Team WHERE teamId = :new.teamId;
+	
+	IF counter >= 9 THEN
+		RAISE tooBigTeam;
+	END IF;
+	
+EXCEPTION
+
+	WHEN tooBigTeam THEN
+		RAISE_APPLICATION_ERROR(-20000, 'Team: ' + :new.teamId + ' is already full.');
+
+END;
+/
+SHOW ERRORS;
+
+
+-- numTeamsLeague trigger checks to make sure that a fantasy league does not get too many fantasy teams
+--		Each fantasy league can only have a maximum of 5 teams
+--		If the INSERT command is going to make the league have more than 6 teams, an application error is raised
+--		notifying the user of the error.
+CREATE OR REPLACE TRIGGER numTeamsLeague BEFORE INSERT ON Team FOR EACH ROW
+DECLARE
+	 counter INTEGER;
+	 tooBigLeague EXCEPTION; 
+BEGIN
+
+	SELECT COUNT(*) INTO counter FROM Team WHERE leagueId = :new.leagueId;
+	
+	IF counter >= 6 THEN
+		RAISE tooBigLeague;
+	END IF;
+	
+EXCEPTION
+
+	WHEN tooBigLeague THEN
+		RAISE_APPLICATION_ERROR(-20000, 'League: ' + :new.leagueId + ' is already full.');
+
+END;
+/
+SHOW ERRORS;
