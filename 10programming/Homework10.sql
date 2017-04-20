4a30486c1a2478b25c396a02923829027b3ffda0
Ethan Clark <Ethan Clark>
2017-04-19 23:07:38 -0400

    Homework10 - still needs comments

--- 10programming/Homework10.sql
+++ 10programming/Homework10.sql
@@ -1,7 +1,7 @@
 -- Homework10.sql
 -- Ethan Clark
 -- CS 342 - Homework 10
--- April 17, 2017
+-- April 19, 2017
 
 -- Write a PL/SQL procedure that receives two movie IDs and an integer amount and “transfers” a given amount of rank value from a source
 --		movie to a destination movie. Note that this doesn’t make much sense in the context of the movie database, but it’s similar to
@@ -13,3 +13,65 @@
 -- Your procedure should check the values it receives, raising exceptions as appropriate.
 -- Don’t allow the rank value to drop below 0.
 -- To make your transaction logic explicit, please use declared exceptions as follows.
+
+CREATE OR REPLACE PROCEDURE transferRank (movieOutOf IN INTEGER, movieInto IN INTEGER, amount IN FLOAT) AS
+
+tooSmallRank EXCEPTION;
+movieOutNotExist EXCEPTION;
+movieInNotExist EXCEPTION;
+foundMovieId INTEGER;
+movieOutRank FLOAT;
+movieIntoRank FLOAT;
+
+BEGIN
+
+	SELECT COUNT(*) INTO foundMovieId FROM Movie WHERE id = movieOutOf;
+
+	IF foundMovieId = 0 THEN
+		RAISE movieOutNotExist;
+	END IF;
+
+	SELECT COUNT(*) INTO foundMovieId FROM Movie WHERE id = movieInto;
+
+	IF foundMovieId = 0 THEN
+		RAISE movieInNotExist;
+	END IF;
+
+	SELECT rank INTO movieOutRank FROM Movie WHERE id = movieOutOf FOR UPDATE;
+
+	IF movieOutRank - amount < 0 THEN
+		RAISE tooSmallRank;
+	END IF;
+
+	UPDATE Movie SET rank = movieOutRank - amount WHERE id = movieOutOf;
+
+	SELECT rank INTO movieIntoRank FROM Movie WHERE id = movieInto FOR UPDATE;
+	UPDATE Movie SET rank = movieIntoRank + amount WHERE id = movieInto;
+
+	COMMIT;
+
+EXCEPTION
+
+	WHEN movieOutNotExist THEN
+		RAISE_APPLICATION_ERROR(-20000, 'Movie id ' || movieOutOf || ' does not exist in the database!');
+
+	WHEN movieInNotExist THEN
+		RAISE_APPLICATION_ERROR(-20000, 'Movie id ' || movieInto || ' does not exist in the database!');
+
+	WHEN tooSmallRank THEN
+		RAISE_APPLICATION_ERROR(-20000, 'Movie id ' || movieOutOf || ' will have rank less than 0!');
+
+END;
+/
+SHOW ERRORS;
+
+
+BEGIN
+	FOR i IN 1..10000 LOOP
+		transferRank(176712, 176711, 0.1);
+		COMMIT;
+		transferRank(176711, 176712, 0.1);
+		COMMIT;
+	END LOOP;
+END;
+/
--- 10programming/Lab10_4.sql
+++ 10programming/Lab10_4.sql
@@ -1,7 +1,7 @@
 -- Lab10_4.sql
 -- Ethan Clark
 -- CS 342 - Lab10
--- April 14, 2017
+-- April 19, 2017
 
 -- Consider the following stored procedure.
 
@@ -12,7 +12,7 @@ CREATE OR REPLACE PROCEDURE incrementRank
 	x Movie.rank%type;
 BEGIN
 	FOR i IN 1..50000 LOOP
-		SELECT rank INTO x FROM Movie WHERE id=movieIdIn;
+		SELECT rank INTO x FROM Movie WHERE id=movieIdIn FOR UPDATE;
 		UPDATE Movie SET rank=x+deltaIn WHERE id=movieIdIn;
 		COMMIT;
 	END LOOP;
@@ -25,4 +25,15 @@ END;
 
 -- Now, determine if it ran correctly. If it does, explain how. If it doesn’t, identify the problem and modify the code to fix it.
 
--- Lab10_4.sql
+
+-- ******* ANSWERS TO QUESTIONS ********
+
+-- The first time I ran this procedure simultaneously on my two SQL*Plus sessions, I executed with the statement
+--		EXECUTE incrementRank(238075, 0.01);
+--		in both sessions to increment the rank of the movie "Ocean's Fourteen" by 0.01 50,000 times each (so the end result of the rank should be 1004.5).
+--		Instead, the updated rank was 623.49, which I have no idea how that happened exactly. Something like the lost update problem where some of the updates
+--		were being lost by the other session overwriting them.
+
+-- To fix this issue, all I did was add the FOR UPDATE locking tool that Oracle provides to lock just the affected rows in the table each time the procedure
+--		needed to update the rank of a movie table record. This allowed the procedure to not loose any update information, and, after running the procedure
+--		again in both of my SQL*Plus sessions, I got a final rank result of 1004.5 for "Ocean's Fourteen".
